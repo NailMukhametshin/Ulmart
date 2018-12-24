518ffce5afefa039e8bf6c1f832a673ebb462985
NailMukhametshin <nail.zubr@gmail.com>
2018-12-25 00:09:25 +0300

    Исправил NameComparator FIXED #2

--- src/main/java/ru/itpark/comparator/NameComparator.java
+++ src/main/java/ru/itpark/comparator/NameComparator.java
@@ -9,6 +9,6 @@ public class NameComparator implements Comparator<Product> {
 
     @Override
     public int compare(Product o1, Product o2) {
-        return  o1.toString().compareTo(o2.toString());
+        return  o1.getName().compareTo(o2.getName());
     }
 }
